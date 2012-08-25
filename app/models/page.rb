5b3d98b23446e88283110643b08419d4b0a5ff4b
RogerGuTeng <answeriversongt@hotmail.com>
2012-08-25 11:19:35 -0400

    initail commit

---
+++ app/models/page.rb
@@ -0,0 +1,5 @@
+class Page
+  include Mongoid::Document
+  field :name, :type => String
+  field :tilte, :type => String
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalWeb::Application.config.secret_token = '0b0af15bedb1c3aaaa7dc62c61a112784dbb15eed02e3a1afc39337602c73bd7bc85bc790fd9b00def06ad0e2493b448d99b1eb70a65ca8adb90bfcdae3fdcfa'
